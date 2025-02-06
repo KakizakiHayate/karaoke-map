317525700e3a4a25b5ca5c2ec7145f41dc89548a
hayate <hayate.k.0704@gmail.com>
2025-02-06 22:43:33 +0900

    上部の背景色を変更

--- lib/home/widgets/search_header_widget.dart
+++ lib/home/widgets/search_header_widget.dart
@@ -2,20 +2,24 @@ import 'package:flutter/material.dart';
 
 class SearchHeaderWidget extends StatelessWidget {
   const SearchHeaderWidget({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16),
-      child: TextField(
-        decoration: InputDecoration(
-          hintText: 'カラオケ店を検索',
-          prefixIcon: const Icon(Icons.search),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(8),
+    return Container(
+      color: Colors.white,
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: TextField(
+          decoration: InputDecoration(
+            hintText: 'カラオケ店を検索',
+            prefixIcon: const Icon(Icons.search),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+            filled: true,
+            fillColor: Colors.grey[200],
           ),
         ),
       ),
     );
   }
-} 
+}
